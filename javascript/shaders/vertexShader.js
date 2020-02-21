ce776213a9e2c575a13300e095868d476312bb42
dani198912 <Feborcesim9>
2020-02-21 13:16:52 +0200

    Added to RGB - testing git

--- javascript/shaders/vertexShader.js
+++ javascript/shaders/vertexShader.js
@@ -19,7 +19,8 @@ void main()	{
     gl_Position =   projectionMatrix * 
                     modelViewMatrix * 
                     vec4(position,1.0);
-    vUv = uv.xy;                    
+    vUv = uv.xy;
+    //added this comment to chck git branch                    
 }
 
 ` // End of shader
